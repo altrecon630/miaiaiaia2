9d1e3c908dfb3c8e25d165058af0201f18baca2b
altrecon630 <altrecon630@gmail.com>
2025-02-10 23:36:37 -0500

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,4 @@
+document.getElementById('userInput').addEventListener('input', function() {
+  const animationContainer = document.getElementById('animationContainer');
+  animationContainer.style.display = 'block';
+});
